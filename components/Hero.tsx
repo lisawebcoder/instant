ef8e8774d5978bd83a71a83bde624b4a9bbbd356
roberto_intern <bellcanadardp@gmail.com>
2025-01-31 04:37:37 -0500

    Removed versionControl folder from project build

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,9 @@ import InputArea from "./InputArea";
 type THeroProps = {
   promptValue: string;
   setPromptValue: React.Dispatch<React.SetStateAction<string>>;
-  handleDisplayResult: () => void;
+  //jan30th2025--having issues on remote server and so i comment out this line and replaced
+  //handleDisplayResult: (query: string) => void; // Modified to accept query directly
+  handleDisplayResult: (query?: string) => void; // Allow optional parameter
 };
 
 const Hero: FC<THeroProps> = ({
@@ -13,14 +15,15 @@ const Hero: FC<THeroProps> = ({
   setPromptValue,
   handleDisplayResult,
 }) => {
+  // Modified: Instead of setting the input value, directly trigger result display
   const handleClickSuggestion = (value: string) => {
-    setPromptValue(value);
+    handleDisplayResult(value);
   };
 
   return (
     <div className="flex flex-col items-center justify-center">
-	{/* jan24th2025--i will comment out from the start of the a tag to the end of the a tag-- */}
-	{/* 
+      {/* Commented out Powered by Together AI section */}
+      {/* 
       <a
         className="mb-4 inline-flex h-7 shrink-0 items-center gap-[9px] rounded-[50px] border-[0.5px] border-solid border-[#E6E6E6] bg-white px-3 py-4 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)]"
         href="https://www.together.ai/"
@@ -37,12 +40,13 @@ const Hero: FC<THeroProps> = ({
           Powered by Together AI
         </span>
       </a>
-	  */}
+      */}
+      
       <h2 className="bg-custom-gradient bg-clip-text pb-7 pt-2 text-center text-3xl font-semibold leading-[normal] lg:text-[64px]">
         Search smarter & faster
       </h2>
 
-      {/* input section */}
+      {/* Input section remains unchanged */}
       <div className="w-full max-w-[708px] pb-6">
         <InputArea
           promptValue={promptValue}
@@ -51,12 +55,12 @@ const Hero: FC<THeroProps> = ({
         />
       </div>
 
-      {/* Suggestions section */}
+      {/* Modified: Suggestions section now directly triggers results */}
       <div className="flex flex-wrap items-center justify-center gap-2.5 pb-[30px] lg:flex-nowrap lg:justify-normal">
         {suggestions.map((item) => (
           <div
             className="flex h-[35px] cursor-pointer items-center justify-center gap-[5px] rounded border border-solid border-[#C1C1C1] bg-[#EDEDEA] px-2.5 py-2"
-            onClick={() => handleClickSuggestion(item?.name)}
+            onClick={() => handleClickSuggestion(item.name)} // Modified: Calls handleDisplayResult directly
             key={item.id}
           >
             <Image
@@ -74,9 +78,8 @@ const Hero: FC<THeroProps> = ({
         ))}
       </div>
 
-      {/* Github link section */}
-	  {/* jan24th2025--i will comment out from the start of the p tag to the end of the p tag-- */}
-	{/* 
+      {/* Commented out GitHub link section */}
+      {/* 
       <p className="text-center text-sm font-light leading-[normal] text-[#1B1B16]">
         Fully open source!{" "}
         <span className="text-sm font-medium underline">
@@ -89,7 +92,7 @@ const Hero: FC<THeroProps> = ({
           </a>
         </span>
       </p>
-	  */}
+      */}
     </div>
   );
 };
@@ -99,28 +102,8 @@ type suggestionType = {
   name: string;
   icon: string;
 };
-{/* jan30th2025--i will comment out from the start of the const var aray to the end of the array-- */}
-{/*
-const suggestions: suggestionType[] = [
-  {
-    id: 1,
-    name: "How does photosynthesis work?",
-    icon: "/img/icon _leaf_.svg",
-  },
-  {
-    id: 2,
-    name: "How can I get a 6 pack in 3 months?",
-    icon: "/img/icon _dumbell_.svg",
-  },
-  {
-    id: 3,
-    name: "Can you explain the theory of relativity?",
-    icon: "/img/icon _atom_.svg",
-  },
-];
-*/}
-{/* jan30th2025--i will adjust suggestions from the start of the const var aray to the end of the array-- */}
 
+// Modified: Updated suggestions to different topics
 const suggestions: suggestionType[] = [
   {
     id: 1,
@@ -138,6 +121,5 @@ const suggestions: suggestionType[] = [
     icon: "/img/icon _atom_.svg",
   },
 ];
-
 
 export default Hero;
